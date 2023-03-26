f700dfb72a255ba2ff2923d4a5cb48c1f6dc6ad7
pegasis <me@pegasis.site>
2023-03-26 01:12:18 -0400

    flash

--- firmware-common/src/common/vlfs.rs
+++ firmware-common/src/common/vlfs.rs
@@ -1,7 +1,14 @@
 use bitvec::prelude::*;
+use defmt::*;
 use heapless::Vec;
 
-use crate::driver::flash::{ReadBuffer, SpiFlash};
+use crate::{
+    driver::{
+        crc::Crc,
+        flash::{ReadBuffer, SpiFlash},
+    },
+    new_read_buffer,
+};
 
 struct FileEntry {
     pub metadata: [u8; 10],
@@ -15,54 +22,112 @@ struct AllocationTable<const MAX_FILES: usize> {
     pub file_entries: Vec<FileEntry, MAX_FILES>,
 }
 
+impl Default for AllocationTable<0> {
+    fn default() -> Self {
+        Self {
+            sequence_number: 0,
+            file_count: 0,
+            file_entries: Vec::new(),
+        }
+    }
+}
+
 const VLFS_VERSION: u32 = 1;
 
-pub struct VLFS<F, const MAX_FILES: usize, const TABLE_COUNT: usize, const SECTORS_COUNT: usize>
+pub struct VLFS<F, C, const MAX_FILES: usize, const TABLE_COUNT: usize, const SECTORS_COUNT: usize>
 where
     F: SpiFlash,
+    C: Crc,
     [u32; SECTORS_COUNT / 32]: Sized,
-    [u8; 12 + MAX_FILES * 12 + 5]: Sized,
+    [u8; 16 + MAX_FILES * 12 + 5]: Sized,
 {
     version: u32,
     allocation_table: AllocationTable<MAX_FILES>,
+    allocation_table_index: usize, // which half block is the allocation table in
     free_sectors: BitArray<[u32; SECTORS_COUNT / 32], Lsb0>,
     flash: F,
+    crc: C,
 }
 
-impl<F, const MAX_FILES: usize, const TABLE_COUNT: usize, const SECTORS_COUNT: usize>
-    VLFS<F, MAX_FILES, TABLE_COUNT, SECTORS_COUNT>
+impl<F, C, const MAX_FILES: usize, const TABLE_COUNT: usize, const SECTORS_COUNT: usize>
+    VLFS<F, C, MAX_FILES, TABLE_COUNT, SECTORS_COUNT>
 where
     F: SpiFlash,
+    C: Crc,
     [u32; SECTORS_COUNT / 32]: Sized,
-    [u8; 12 + MAX_FILES * 12 + 5]: Sized,
+    [u8; 16 + MAX_FILES * 12 + 5]: Sized,
 {
     pub fn new() -> Self {
-        assert!(12 + MAX_FILES * 12 < 32 * 1024);
-        todo!()
+        defmt::assert!(16 + MAX_FILES * 12 < 32 * 1024);
+        defmt::todo!()
     }
 
-    pub async fn read_allocation_table(flash: &mut F) -> Result<(), ()> {
-        let tables: Vec<AllocationTable<MAX_FILES>, TABLE_COUNT> = Vec::new();
-        let mut read_buffer: ReadBuffer<{ 12 + MAX_FILES * 12 }, 5> = ReadBuffer::new();
+    async fn read_allocation_table(&mut self) -> Option<AllocationTable<MAX_FILES>> {
+        let mut table: Option<AllocationTable<MAX_FILES>> = None;
+        new_read_buffer!(read_buffer, 16 + MAX_FILES * 12);
+
         for i in 0..TABLE_COUNT {
             read_buffer.reset();
-            flash.read(
+            self.flash.read(
                 (i * 32 * 1024).try_into().unwrap(),
                 read_buffer.len(),
                 &mut read_buffer,
-            );
+            ).await;
 
-            
+            let expected_crc = read_buffer.read_u32();
+            let actual_crc = self.crc.calculate(&read_buffer.as_slice_without_start()[4..]);
+            if expected_crc != actual_crc {
+                warn!("CRC mismatch for allocation table #{}", i);
+                continue;
+            }
 
-            // let mut buffer = flash.read_256_bytes(i * 256).await;
-            // let length = buffer.read_u32() as usize;
-            // let table = deserialize_safe!(AllocationTable<MAX_FILES>, buffer.read_slice(length));
-            // if let Some(table) = table {
-            //     tables.push(table);
-            // }
+            let version = read_buffer.read_u32();
+            if version != VLFS_VERSION {
+                warn!("Version mismatch for allocation table #{}", i);
+                continue;
+            }
+
+            let sequence_number = read_buffer.read_u32();
+            let file_count = read_buffer.read_u32();
+            let mut files: Vec<FileEntry, MAX_FILES> = Vec::new();
+            for _ in 0..file_count {
+                let metadata = read_buffer.read_slice(10);
+                let metadata: [u8; 10] = metadata.try_into().unwrap();
+                let first_sector_index = read_buffer.read_u16();
+                files
+                    .push(FileEntry {
+                        metadata,
+                        first_sector_index,
+                    })
+                    .ok()
+                    .unwrap();
+            }
+
+            if let Some(last_table) = table.take() {
+                if sequence_number > last_table.sequence_number {
+                    table.replace(AllocationTable {
+                        sequence_number,
+                        file_count,
+                        file_entries: files,
+                    });
+                } else {
+                    table.replace(last_table);
+                }
+            } else {
+                table.replace(AllocationTable {
+                    sequence_number,
+                    file_count,
+                    file_entries: files,
+                });
+            }
         }
 
-        Ok(())
+        table
+    }
+
+    async fn write_allocation_table(&mut self) {
+        self.allocation_table_index = (self.allocation_table_index + 1) % TABLE_COUNT;
+        
     }
 }
 
--- firmware-common/src/driver/flash.rs
+++ firmware-common/src/driver/flash.rs
@@ -1,3 +1,5 @@
+use defmt::*;
+
 pub trait SpiFlash {
     // size in bytes
     fn size(&self) -> u32;
@@ -6,35 +8,91 @@ pub trait SpiFlash {
     async fn erase_block_32kib(&mut self, address: u32);
     async fn erase_block_64kib(&mut self, address: u32);
 
-    // read arbitary length, N must be larger or equal to read_length
-    async fn read<'b, const N: usize>(
+    // maximum read length is 4 kb
+    // size of the buffer must be at least 5 bytes larger than read_length
+    async fn read_4kib<'b>(
         &mut self,
         address: u32,
         read_length: usize,
-        read_buffer: &'b mut ReadBuffer<N, 5>,
-    ) where
-        [u8; N + 5]: Sized;
+        read_buffer: &'b mut [u8],
+    ) -> &'b [u8];
 
     // Write a full page of 256 bytes, the last byte of the address is ignored
-    // The write buffer must be 261 bytes long, where the last 256 bytes are the data to write
-    async fn write_page<'b>(&mut self, address: u32, write_buffer: &'b mut WriteBuffer);
+    // The write buffer must be at least 261 bytes long, where the last 256 bytes are the data to write
+    async fn write_256b<'b>(&mut self, address: u32, write_buffer: &'b mut [u8]);
+
+    // read arbitary length, N must be larger or equal to read_length
+    async fn read<'b, 'c, const N: usize>(
+        &mut self,
+        address: u32,
+        read_length: usize,
+        read_buffer: &'b mut ReadBuffer<'c, N, 5>,
+    ) where
+        [u8; N + 5]: Sized,
+    {
+        let read_buffer = read_buffer.as_mut_slice();
+        let mut bytes_read = 0;
+        while bytes_read < read_length {
+            let length = if read_length - bytes_read > 4096 {
+                4096
+            } else {
+                read_length - bytes_read
+            };
+            info!("reading {}/{} bytes", bytes_read, read_length);
+            self.read_4kib(
+                address + bytes_read as u32,
+                length,
+                &mut read_buffer[bytes_read..],
+            )
+            .await;
+            bytes_read += length;
+        }
+    }
+
+    // write arbitary length (must be a multiple of 256 bytes)
+    // address must be 256-byte-aligned
+    // N must be larger or equal to read_length
+    async fn write<'b, 'c, const N: usize>(
+        &mut self,
+        address: u32,
+        write_length: usize,
+        write_buffer: &'b mut WriteBuffer<'c, N, 5>,
+    ) where
+        [u8; N + 5]: Sized,
+    {
+        let write_buffer = write_buffer.as_mut_slice();
+        let mut bytes_written = 0;
+        while bytes_written < write_length {
+            let length = if write_length - bytes_written > 256 {
+                256
+            } else {
+                write_length - bytes_written
+            };
+            info!("writing {}/{} bytes", bytes_written, write_length);
+            self.write_256b(
+                address + bytes_written as u32,
+                &mut write_buffer[bytes_written..],
+            ).await;
+            bytes_written += length;
+        }
+    }
 }
 
-pub struct ReadBuffer<const N: usize, const START_OFFSET: usize>
+pub struct ReadBuffer<'a, const N: usize, const START_OFFSET: usize>
 where
     [u8; N + START_OFFSET]: Sized,
 {
-    buffer: [u8; N + START_OFFSET],
+    buffer: &'a mut [u8; N + START_OFFSET],
     offset: usize,
 }
 
-impl<const N: usize, const START_OFFSET: usize> ReadBuffer<N, START_OFFSET>
+impl<'a, const N: usize, const START_OFFSET: usize> ReadBuffer<'a, N, START_OFFSET>
 where
     [u8; N + START_OFFSET]: Sized,
 {
-    pub fn new() -> Self {
+    pub fn new(buffer: &'a mut [u8; N + START_OFFSET]) -> Self {
         Self {
-            buffer: [0u8; N + START_OFFSET],
+            buffer,
             offset: START_OFFSET,
         }
     }
@@ -53,6 +111,19 @@ where
         }
         let value = self.buffer[self.offset];
         self.offset += 1;
+        value
+    }
+
+    pub fn read_u16(&mut self) -> u16 {
+        if 2 > self.buffer.len() - self.offset {
+            return 0;
+        }
+        let value = u16::from_be_bytes(
+            self.buffer[self.offset..(self.offset + 2)]
+                .try_into()
+                .unwrap(),
+        );
+        self.offset += 2;
         value
     }
 
@@ -84,26 +155,47 @@ where
         }
     }
 
+    pub fn as_slice_without_start(&mut self) -> &[u8] {
+        &self.buffer[START_OFFSET..]
+    }
+
     pub fn as_mut_slice(&mut self) -> &mut [u8; N + START_OFFSET] {
         &mut self.buffer
     }
 }
 
-pub struct WriteBuffer {
-    buffer: [u8; 261],
+#[macro_export]
+macro_rules! new_read_buffer {
+    ($name: ident, $length:expr, $start:expr) => {
+        let mut $name = [0u8; $length + $start];
+        let mut $name: ReadBuffer<{ $length }, $start> = ReadBuffer::new(&mut $name);
+    };
+    ($name: ident, $length:expr) => {
+        new_read_buffer!($name, $length, 5);
+    };
+}
+
+pub struct WriteBuffer<'a, const N: usize, const START_OFFSET: usize>
+where
+    [u8; N + START_OFFSET]: Sized,
+{
+    buffer: &'a mut [u8; N + START_OFFSET],
     offset: usize,
 }
 
-impl WriteBuffer {
-    pub fn new() -> Self {
+impl<'a, const N: usize, const START_OFFSET: usize> WriteBuffer<'a, N, START_OFFSET>
+where
+    [u8; N + START_OFFSET]: Sized,
+{
+    pub fn new(buffer: &'a mut [u8; N + START_OFFSET]) -> Self {
         Self {
-            buffer: [0u8; 261],
-            offset: 5,
+            buffer,
+            offset: START_OFFSET,
         }
     }
 
     pub fn reset(&mut self) {
-        self.offset = 5;
+        self.offset = START_OFFSET;
     }
 
     pub fn extend_from_slice(&mut self, slice: &[u8]) {
@@ -114,8 +206,17 @@ impl WriteBuffer {
         self.offset += slice.len();
     }
 
+    pub fn extend_from_u8(&mut self, value: u8) {
+        self.buffer[self.offset] = value;
+        self.offset += 1;
+    }
+
     pub fn extend_from_u32(&mut self, value: u32) {
         self.extend_from_slice(&value.to_be_bytes());
+    }
+
+    pub fn replace_u32(&mut self, value: u32, i: usize) {
+        self.buffer[i..(i + 4)].copy_from_slice(&value.to_be_bytes());
     }
 
     pub fn align_4_bytes(&mut self) {
@@ -124,7 +225,18 @@ impl WriteBuffer {
         }
     }
 
-    pub fn as_mut_slice(&mut self) -> &mut [u8; 261] {
+    pub fn as_mut_slice(&mut self) -> &mut [u8; N + START_OFFSET] {
         &mut self.buffer
     }
+}
+
+#[macro_export]
+macro_rules! new_write_buffer {
+    ($name: ident, $length:expr, $start:expr) => {
+        let mut $name = [0u8; $length + $start];
+        let mut $name: WriteBuffer<{ $length }, $start> = WriteBuffer::new(&mut $name);
+    };
+    ($name: ident, $length:expr) => {
+        new_write_buffer!($name, $length, 5);
+    };
 }
