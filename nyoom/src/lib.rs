43f16a132dedd39200d3910fc11e937e6d464cad
pegasis <me@pegasis.site>
2023-05-02 17:59:46 -0400

    update lib

--- nyoom/src/lib.rs
+++ nyoom/src/lib.rs
@@ -5,6 +5,7 @@ use embassy_sync::{
     pipe::{Reader, Writer},
 };
 
+// Nyoom V3 Encoder
 pub struct NyoomEncoder {}
 
 impl NyoomEncoder {
@@ -12,7 +13,12 @@ impl NyoomEncoder {
         todo!()
     }
 
-    async fn addHandshake<'r, 'w, MR, const NR: usize, MW, const NW: usize>(
+    // Function to encode a handshake message.
+    // The `handshake` parameter contains all the handshake information except the rocket model
+    // The `rocket_model_reader` allows this function to read the model byte-by-byte,
+    //   instead of reading the entire model at once, which will overwhelme the RAM.
+    // The encoded message should be written to `output_writer`.
+    async fn encodeHandshake<'r, 'w, MR, const NR: usize, MW, const NW: usize>(
         &mut self,
         handshake: (), // TODO
         rocket_model_reader: Reader<'r, MR, NR>,
@@ -24,7 +30,10 @@ impl NyoomEncoder {
         todo!()
     }
 
-    fn addData<'r, 'w, MW, const NW: usize>(
+    // Function to encode a data message.
+    // The `data parameter contains all the information to be encoded
+    // The encoded message should be written to `output_writer`.
+    fn encodeData<'r, 'w, MW, const NW: usize>(
         &mut self,
         data: (), // TODO
         output_writer: Writer<'w, MW, NW>,
