ded93e7b99c9e96d1ad67f1104845bff6d4bab02
pegasis <me@pegasis.site>
2024-06-11 11:50:26 -0400

    vl cli project

---
+++ vl-cli/src/main.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    for port in tokio_serial::available_ports()? {
+        println!("{:?}", port);
+    }
+
+    let mut rl = DefaultEditor::new()?;
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).ok();
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").ok();
+    Ok(())
+}
