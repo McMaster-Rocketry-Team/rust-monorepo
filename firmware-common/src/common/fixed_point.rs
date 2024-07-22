6ef07476267fdf38572ae87ac1564b498308045e
Pegasis <me@pegasis.site>
2024-07-21 23:33:09 -0400

    new fixed point factory

--- firmware-common/src/common/fixed_point.rs
+++ firmware-common/src/common/fixed_point.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+use packed_struct::prelude::*;
+
 #[macro_export]
 macro_rules! fixed_point_factory {
     ($name:ident, $min:expr, $max:expr, $float:ident, $fixed:ident) => {
@@ -35,34 +38,148 @@ macro_rules! fixed_point_factory {
     };
 }
 
+pub struct FixedPointFactory<Source, const TARGET_BITS: usize> {
+    pub min: Source,
+    pub max: Source,
+}
+
+macro_rules! fixed_point_factory_impl {
+    ($source:ty, $target_base: ty, $target_bits: expr) => {
+        impl FixedPointFactory<$source, $target_bits> {
+            pub fn new(min: $source, max: $source) -> Self {
+                Self { min, max }
+            }
+
+            fn target_max() -> $target_base {
+                (1 as $target_base)
+                    .checked_shl($target_bits)
+                    .unwrap_or(0)
+                    .wrapping_sub(1)
+            }
+
+            pub fn to_fixed_point(
+                &self,
+                value: $source,
+            ) -> Option<Integer<$target_base, packed_bits::Bits<$target_bits>>> {
+                if value < self.min || value > self.max {
+                    return None;
+                }
+                let value = value - self.min;
+                let value = value / (self.max - self.min);
+                let value = value * Self::target_max() as $source;
+                Some((value as $target_base).into())
+            }
+
+            pub fn to_fixed_point_capped(
+                &self,
+                value: $source,
+            ) -> Integer<$target_base, packed_bits::Bits<$target_bits>> {
+                let value = if value < self.min {
+                    self.min
+                } else if value > self.max {
+                    self.max
+                } else {
+                    value
+                };
+                return self.to_fixed_point(value).unwrap();
+            }
+
+            pub fn to_float(
+                &self,
+                value: Integer<$target_base, packed_bits::Bits<$target_bits>>,
+            ) -> $source {
+                let value: $target_base = value.into();
+                let value = value as $source;
+                let value = value / Self::target_max() as $source;
+                let value = value * (self.max - self.min);
+                value + self.min
+            }
+        }
+    };
+}
+
+fixed_point_factory_impl!(f64, u8, 2);
+fixed_point_factory_impl!(f64, u8, 3);
+fixed_point_factory_impl!(f64, u8, 4);
+fixed_point_factory_impl!(f64, u8, 5);
+fixed_point_factory_impl!(f64, u8, 6);
+fixed_point_factory_impl!(f64, u8, 7);
+fixed_point_factory_impl!(f64, u8, 8);
+fixed_point_factory_impl!(f64, u16, 9);
+fixed_point_factory_impl!(f64, u16, 10);
+fixed_point_factory_impl!(f64, u16, 11);
+fixed_point_factory_impl!(f64, u16, 12);
+fixed_point_factory_impl!(f64, u16, 13);
+fixed_point_factory_impl!(f64, u16, 14);
+fixed_point_factory_impl!(f64, u16, 15);
+fixed_point_factory_impl!(f64, u16, 16);
+fixed_point_factory_impl!(f64, u32, 17);
+fixed_point_factory_impl!(f64, u32, 18);
+fixed_point_factory_impl!(f64, u32, 19);
+fixed_point_factory_impl!(f64, u32, 20);
+fixed_point_factory_impl!(f64, u32, 21);
+fixed_point_factory_impl!(f64, u32, 22);
+fixed_point_factory_impl!(f64, u32, 23);
+fixed_point_factory_impl!(f64, u32, 24);
+fixed_point_factory_impl!(f64, u32, 25);
+fixed_point_factory_impl!(f64, u32, 26);
+fixed_point_factory_impl!(f64, u32, 27);
+fixed_point_factory_impl!(f64, u32, 28);
+fixed_point_factory_impl!(f64, u32, 29);
+fixed_point_factory_impl!(f64, u32, 30);
+fixed_point_factory_impl!(f64, u32, 31);
+fixed_point_factory_impl!(f64, u32, 32);
+
+fixed_point_factory_impl!(f32, u8, 2);
+fixed_point_factory_impl!(f32, u8, 3);
+fixed_point_factory_impl!(f32, u8, 4);
+fixed_point_factory_impl!(f32, u8, 5);
+fixed_point_factory_impl!(f32, u8, 6);
+fixed_point_factory_impl!(f32, u8, 7);
+fixed_point_factory_impl!(f32, u8, 8);
+fixed_point_factory_impl!(f32, u16, 9);
+fixed_point_factory_impl!(f32, u16, 10);
+fixed_point_factory_impl!(f32, u16, 11);
+fixed_point_factory_impl!(f32, u16, 12);
+fixed_point_factory_impl!(f32, u16, 13);
+fixed_point_factory_impl!(f32, u16, 14);
+fixed_point_factory_impl!(f32, u16, 15);
+fixed_point_factory_impl!(f32, u16, 16);
+fixed_point_factory_impl!(f32, u32, 17);
+fixed_point_factory_impl!(f32, u32, 18);
+fixed_point_factory_impl!(f32, u32, 19);
+fixed_point_factory_impl!(f32, u32, 20);
+fixed_point_factory_impl!(f32, u32, 21);
+fixed_point_factory_impl!(f32, u32, 22);
+fixed_point_factory_impl!(f32, u32, 23);
+fixed_point_factory_impl!(f32, u32, 24);
+fixed_point_factory_impl!(f32, u32, 25);
+fixed_point_factory_impl!(f32, u32, 26);
+fixed_point_factory_impl!(f32, u32, 27);
+fixed_point_factory_impl!(f32, u32, 28);
+fixed_point_factory_impl!(f32, u32, 29);
+fixed_point_factory_impl!(f32, u32, 30);
+fixed_point_factory_impl!(f32, u32, 31);
+fixed_point_factory_impl!(f32, u32, 32);
+
 #[cfg(test)]
 mod test {
     use approx::assert_relative_eq;
+
+    use crate::common::fixed_point::FixedPointFactory;
 
     fixed_point_factory!(FixedPointFactoryf64u16, 0.0, 1.0, f64, u16);
 
     #[test]
     fn test_fixed_point_factory() {
-        assert_eq!(
-            FixedPointFactoryf64u16::to_fixed_point(0.0),
-            Some(0)
-        );
-        assert_eq!(
-            FixedPointFactoryf64u16::to_fixed_point(0.5),
-            Some(32767)
-        );
-        assert_eq!(
-            FixedPointFactoryf64u16::to_fixed_point(1.0),
-            Some(65535)
-        );
-        assert_eq!(FixedPointFactoryf64u16::to_fixed_point(-1.0), None);
-        assert_eq!(FixedPointFactoryf64u16::to_fixed_point(2.0), None);
+        let factory = FixedPointFactory::<f64, 16>::new(0.0, 1.0);
+        assert_eq!(factory.to_fixed_point(0.0), Some(0.into()));
+        assert_eq!(factory.to_fixed_point(0.5), Some(32767.into()));
+        assert_eq!(factory.to_fixed_point(1.0), Some(65535.into()));
+        assert_eq!(factory.to_fixed_point(-1.0), None);
+        assert_eq!(factory.to_fixed_point(2.0), None);
 
-        assert_relative_eq!(
-            FixedPointFactoryf64u16::to_float(0),
-            0.0,
-            epsilon = 0.0001
-        );
+        assert_relative_eq!(FixedPointFactoryf64u16::to_float(0), 0.0, epsilon = 0.0001);
         assert_relative_eq!(
             FixedPointFactoryf64u16::to_float(32767),
             0.5,
